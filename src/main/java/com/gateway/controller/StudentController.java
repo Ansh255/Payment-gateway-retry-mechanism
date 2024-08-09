166020fdbd561d1af99e2671f1c9c7b7f5b33728
Ansh255 <anshgandhi.21.cs@iite.indusuni.ac.in>
2024-08-09 16:28:58 +0530

    Updated Index pages and Libaray and controller

--- src/main/java/com/gateway/controller/StudentController.java
+++ src/main/java/com/gateway/controller/StudentController.java
@@ -1,4 +1,15 @@
 package com.gateway.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StudentController {
+
+    @GetMapping("/")
+    public String init(){
+        return "index";
+    }
+
+
 }
