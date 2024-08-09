77c483c8fdf24dde66231e747d04ab0695a652e4
Ansh255 <anshgandhi.21.cs@iite.indusuni.ac.in>
2024-08-09 14:49:40 +0530

    Updated StudentOrderRepo.java

--- src/main/java/com/gateway/dao/StudentOrderRepo.java
+++ src/main/java/com/gateway/dao/StudentOrderRepo.java
@@ -1,7 +1,8 @@
 package com.gateway.dao;
 
+import com.gateway.dto.StudentOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentOrderRepo extends JpaRepository {
+public interface StudentOrderRepo extends JpaRepository<StudentOrder,Integer> {
 
 }
