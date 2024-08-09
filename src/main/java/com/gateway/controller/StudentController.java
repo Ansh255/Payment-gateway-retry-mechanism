4ff758720cf5ca9340bc10624af6977f17d8b6b9
Ansh255 <anshgandhi.21.cs@iite.indusuni.ac.in>
2024-08-09 21:23:09 +0530

    updated index.html

--- src/main/java/com/gateway/controller/StudentController.java
+++ src/main/java/com/gateway/controller/StudentController.java
@@ -1,7 +1,13 @@
 package com.gateway.controller;
 
+import com.gateway.dto.StudentOrder;
+import com.gateway.service.StudentService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -10,6 +16,11 @@ public class StudentController {
     public String init(){
         return "index";
     }
+
+//    private StudentOrder studentOrder;
+
+    @Autowired
+    private StudentService studentService;
 
 
 }
--- src/main/java/com/gateway/service/StudentService.java
+++ src/main/java/com/gateway/service/StudentService.java
@@ -1,4 +1,26 @@
 package com.gateway.service;
 
+import com.gateway.dao.StudentOrderRepo;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.gateway.dto.StudentOrder;
+
+@Service
 public class StudentService {
+    @Autowired
+    private StudentOrderRepo studentOrderRepo;
+
+    @Value("${razorpay.key.id}")
+    private String razorPayKey;
+    @Value("${razorpay.secret.key}")
+    private String razorPaySecret;
+
+    private RazorpayClient client;
+
+
+
 }
